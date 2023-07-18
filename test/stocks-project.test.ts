10483615bd59fea7db32ec6fc558c8105a0845e6
VSai2314108 <84807052+VSai2314108@users.noreply.github.com>
2023-07-18 12:27:51 -0700

    Initial commit

---
+++ test/stocks-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StocksProject from '../lib/stocks-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stocks-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StocksProject.StocksProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
